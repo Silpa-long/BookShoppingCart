0b6b7af70302f53cd73f03bef01c0b9dc8dc2c0d
palla123 <pallapusilpa@gmail.com>
2025-01-10 21:42:30 -0500

    changes

---
+++ BooksShoopingCart/BooksShoopingCart/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BooksShoopingCart.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
